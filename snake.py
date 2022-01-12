b9223ab7196dd507b10a41b37208a5ddcdb5b7f1
Gherghiceanu Bogdan <bogdan.gherghiceanu1@gmail.com>
2022-01-12 04:40:56 +0200

    create game window

--- snake.py
+++ snake.py
@@ -1,12 +1,16 @@
 import pygame
 
+WIDTH_START = 400
+HEIGHT_START =600
+WIN = pygame.display.set_mode((WIDTH_START,HEIGHT_START))
+
 def main():
 
     #initialize menu
     run_app=True
     page=0
     while run_app:
-
+        pygame.display.set_caption("Snake Menu")
         if page==-1:
             run_app = False
         if page==0:
